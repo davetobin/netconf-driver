4cb2b151270d3b87fbf4de204fb69d161734ce85
Akshata-Desai <Akshata.Desai@ibm.com>
2023-03-24 06:19:12 -0700

    Resolved logging issues in Netconf-drivers

--- netconfdriver/service/operations/config_operations.py
+++ netconfdriver/service/operations/config_operations.py
@@ -30,16 +30,14 @@ class ConfigOperations():
     def netconf_edit(conn,package_properties,default_operation,request_id):
         try:
             external_request_id = str(uuid.uuid4())
-            ConfigOperations._generate_additional_logs(package_properties, 'sent',external_request_id,'application/xml',
-                                       'request', 'netconf', {'default-operation' : default_operation}, request_id)
+            ConfigOperations._generate_additional_logs(package_properties, 'sent', external_request_id,
+                                       'message', 'rpc', {'operation' : default_operation}, request_id)
             edit_config_details = conn.edit_config(config=package_properties, target="running", default_operation=default_operation)
-            ConfigOperations._generate_additional_logs(edit_config_details, 'received', external_request_id,'application/xml',
-                                       'response', 'netconf', {'error' : '','errors' : edit_config_details.errors,'ok' : edit_config_details.ok }, request_id)
+            ConfigOperations._generate_additional_logs(edit_config_details, 'received', external_request_id,
+                                       'message', 'rpc', {'operation' : default_operation}, request_id)
             logger.debug('config_details = %s', edit_config_details)
             return edit_config_details
         except Exception as e:
-            ConfigOperations._generate_additional_logs(e, 'received',external_request_id,'plain/text',
-                                       'response', 'netconf', {'error-tag' : e.tag,'error-info' : '','error-severity' : e.severity,'error-path' : e.path,'error-type' : e.type}, request_id)
             logger.error('Unexpected exception {0}'.format(e))
             raise NetconfConfigError(str(e)) from e
         
@@ -51,14 +49,13 @@ class ConfigOperations():
             logger.error('Unexpected exception {0}'.format(e))
             raise NetconfConfigError(str(e)) from e
 
-
-    def _generate_additional_logs(message_data, message_direction, external_request_id,content_type,
+    def _generate_additional_logs(rpc, message_direction, external_request_id,
                                   message_type, protocol, protocol_metadata, driver_request_id):
         try:
             logging_context_dict = {'message_direction' : message_direction, 'tracectx.externalrequestid' : external_request_id,
-                                    'message_type' : message_type, 'content_type' : content_type, 'protocol' : protocol, 'protocol_metadata' : str(protocol_metadata).replace("'", '\"'), 'tracectx.driverrequestid' : driver_request_id}
+                                    'message_type' : message_type, 'protocol' : protocol, 'protocol_metadata' : protocol_metadata, 'tracectx.driverrequestid' : driver_request_id}
             logging_context.set_from_dict(logging_context_dict)
-            logger.info(str(message_data).replace("'",'\"'))
+            logger.info(rpc)
         finally:
             if('message_direction' in logging_context.data):
                 logging_context.data.pop("message_direction")
